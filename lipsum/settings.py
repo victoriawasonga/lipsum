24beb481fff80c372867c3805b1a9d17b4208a84
unknown <elizabethokumu92@gmail.com>
2022-03-21 21:34:11 +0300

    Added static files

--- lipsum/settings.py
+++ lipsum/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lipsum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIR=[os.path.join(BASE_DIR,'lipsum/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
